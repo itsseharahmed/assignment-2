b9626d4d807acb83cb405629db2e0b1023cb76df
itsseharahmed <94392173+itsseharahmed@users.noreply.github.com>
2021-12-03 22:56:27 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,30 +1,48 @@
-public class Student extends Person{   
-  private String id;
-  private String name;
-  private Vector courses; // contains all courses the student is registered in
+import java.util.Vector;
+import java.util.HashSet;
 
-
-	public Student(String stdName, String stdId) { 
-	   
+public class Student extends Person
+{
+	private String id;
+	private Vector<Course> courses; // contains all courses the student is registered in
+	public String getName() {
+		return name;
+	}
+	public String getId() {
+		return id;
+	}
+	public Student(String id, String name) {
+	// initialize name and id. Also initialize the Vector
+		this.id = id;
+		this.name = name;
+		courses = new Vector<>();
 	}
 
+	// registering the student for the given course
+	public void registerFor(Course course) {
+		if (!courses.contains(course)) {
+			courses.add(course);
+			course.classList.add(this);
+			course.getDept().registerList.add(this);
+		}
+	}
 
-  	  public String getName() {  		 
-  		 	 
-  	  }
+	public boolean isRegisteredInCourse(Course course) {
+		return courses.contains(course);
+	}
 
-  	  public String getId() {
-  		  
-  	  } 	  
-  	  
-  	  
-  	
+	public String toString()
+  {
+	// return a string representation of a student using the following format:
+	// 100234546 John McDonald
+	// Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
 
-    public String toString() {
-     // return a string representation of a student using the following format:
-     // 100234546 John McDonald
-     // Registered courses: ELEE 2110, ELEE 2790, SOFE 2710, SOFE 2800, SOFE 2850
-   
-	return null; 
-  }
+		StringBuilder sb = new StringBuilder(id).append(" ").append(name).append("\nRegistered courses: ");
+		for (Course course : courses)
+    {
+			sb.append(", ");
+			sb.append(course.getCode()).append(" ").append(course.getTitle());
+		}
+		return sb.toString();
+	}
 }
