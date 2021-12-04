7af812e3f9a6bc15f24e432d16258c8fb8ac5737
itsseharahmed <94392173+itsseharahmed@users.noreply.github.com>
2021-12-03 22:54:09 -0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,31 +1,42 @@
+import java.util.Vector;
 
+public class Course {
+  //variable declarations
+    private Department dept;
+    private String title;
+    private String code;
+    private int number;
+    Vector<Student> classList;
 
-public class Course{
-   private Department dept;
-   private String title; // title of the course (e.g. Intro to programming);
-   private String code; // course code, e.g. SOFE, ELEE, MANE, etc.
-   private int number; // course number, e.g. 1200, 2710, 2800, etc.
-   private  Vector <Student>classList; // contains all students registered in this course
+    public Department getDept()
+    {
+        return dept;
+    }
+    public String getCode()
+    {
+        return code;
+    }
+    public int getNumber()
+    {
+        return number;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public Course(String code, int number, Department dept, String title)
+    {
+        this.code = code;
+        this.number = number;
+        this.dept = dept;
+        this.title = title;
+        //classList initialisaiton
+        classList = new Vector<>();
+    }
 
-   public Course(String code, int number, Department dept, String title) {
-	      // also initialize the classList;
-	 	   
-	   }
-
-   
-   public Course() {
-	// TODO Auto-generated constructor stub
-}
- 
-  public String toString() {
-     // return a string representation of the course with the course code,
-     // name, and number of people registered in the course in the following
-     // format:
-     // SOFE 2710 Object Oriented Programming and Design, Enrollment = 260
-
-     return null;
-  }
-
-
-
+    public String toString()
+    {
+        // returns a string representation of the course details
+        return code + " " + number + " " + title + ", Enrollment = " + String.valueOf(classList.size());
+    }
 }
